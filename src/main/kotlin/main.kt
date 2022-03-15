421f5e02f7673198cda80b42efbd34af9cd5de77
Pinkie Aruwani <aruwanip@gmail.com>
2022-03-14 21:28:54 -0400

    Kotlin Number types

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 fun main() {
-    println("Hello World")
-    variables()
+//    println("Hello World")
+//    variables()
+    numberTypes()
 }
 
 fun variables() {
@@ -29,4 +30,25 @@ fun variables() {
     println(name::class)
 
     var currentAge: Int = 32
+}
+
+fun numberTypes() {
+    val myByte: Byte = 8 // 8-bit signed integer
+    val myShort: Short = 16 // 16-bit signed integer
+    val myInt: Int = 32 // 32-bit signed integer
+    val myLong: Long = 64 // 64-bit signed integer
+
+    // Decimals
+    val myFloat: Float = 32.00F // 32-bit floating point number
+    val myDouble: Double = 64.00 // 64-bit floating point number
+
+    // All number types in Kotlin are objects and therefore have methods
+    println(myByte.toDouble())
+    println(myByte.toLong()::class)
+
+    val bigLong: Long = 1_000_000
+    println(bigLong)
+
+    println(myInt.plus(12))
+    println(myInt.minus(12))
 }
