92becab87f5751154a0e0a5b27a9dfa6d4853e14
Pinkie Aruwani <aruwanip@gmail.com>
2022-03-20 19:58:20 -0400

    Abstract functions in Kotlin enums

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -457,26 +457,39 @@ class Vehicle {
 }
 
 fun enums() {
-    println(AccountType.GOLD)
-    val accountTYpeFromApi = "gold"
-    val accountType = AccountType.valueOf(accountTYpeFromApi.uppercase())
-    println(accountType == AccountType.GOLD)
-
-    val user = User("Jane", "Doe", AccountType.PLATINUM)
-
-    println(AccountType.GOLD)
-    println(AccountType.GOLD.discountPercent)
-    println(AccountType.GOLD.numberOfSubscriptions)
-
-    val value = AccountType.valueOf("Platinum".uppercase())
-    println(value)
-    println(value.discountPercent)
-    println(value.numberOfSubscriptions)
+//    println(AccountType.GOLD)
+//    val accountTYpeFromApi = "gold"
+//    val accountType = AccountType.valueOf(accountTYpeFromApi.uppercase())
+//    println(accountType == AccountType.GOLD)
+//
+//    val user = User("Jane", "Doe", AccountType.PLATINUM)
+//
+//    println(AccountType.GOLD)
+//    println(AccountType.GOLD.discountPercent)
+//    println(AccountType.GOLD.numberOfSubscriptions)
+//
+//    val value = AccountType.valueOf("Platinum".uppercase())
+//    println(value)
+//    println(value.discountPercent)
+//    println(value.numberOfSubscriptions)
+    val fromApi = "BroNZe"
+    val accountType = AccountType.valueOf(fromApi.uppercase())
+    println(accountType.calculateDiscountPercent())
 }
 
-enum class AccountType(val discountPercent: Int, val numberOfSubscriptions: Int) {
-    BRONZE(10, 1),
-    SILVER(15, 5),
-    GOLD(20, 10),
-    PLATINUM(25, 15)
+enum class AccountType() {
+    BRONZE {
+        override fun calculateDiscountPercent() = 5
+    },
+    SILVER {
+        override fun calculateDiscountPercent() = 10
+    },
+    GOLD {
+        override fun calculateDiscountPercent() = 15
+    },
+    PLATINUM {
+        override fun calculateDiscountPercent() = 20
+    };
+
+    abstract fun calculateDiscountPercent(): Int
 }
